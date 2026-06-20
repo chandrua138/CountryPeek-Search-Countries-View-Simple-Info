15f6b567345153bcd45ac01e30de7f95e75d8f81
chandrua138 <chandru.a.s.138@kalvium.community>
2026-06-20 10:39:21 +0530

    Add favourites feature

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,7 +1,20 @@
 import { Link } from 'react-router-dom'
+import { useFavourites } from '../context/FavouritesContext'
 
 function CountryCard({ country }) {
   const { name, flags, population, region, capital, cca3 } = country
+  const { favourites, dispatch } = useFavourites()
+  const isSaved = favourites.some((fav) => fav.cca3 === cca3)
+
+  const handleFavouriteClick = (e) => {
+    e.stopPropagation()
+    if (isSaved) {
+      dispatch({ type: 'REMOVE_FAVOURITE', payload: cca3 })
+      return
+    }
+
+    dispatch({ type: 'ADD_FAVOURITE', payload: country })
+  }
 
   return (
     <Link to={`/country/${cca3}`} className="card">
@@ -17,6 +30,13 @@ function CountryCard({ country }) {
         <p>
           <span>Capital:</span> {capital?.[0] ?? 'N/A'}
         </p>
+        <button
+          type="button"
+          className={isSaved ? 'fav-btn fav-btn--saved' : 'fav-btn'}
+          onClick={handleFavouriteClick}
+        >
+          {isSaved ? '♥ Saved' : '♡ Save'}
+        </button>
       </div>
     </Link>
   )
